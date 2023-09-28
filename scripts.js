c7f2db395f4dc8295318fc616f0ec49e219c422a
Edwards67 <129787412+Edwards67@users.noreply.github.com>
2023-09-28 17:26:01 +0000

    updated verison

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log('Hello World!') //wakes up console also has to be set up in index.html
+//console.log('Hello World!') //wakes up console also has to be set up in index.html
+// Check JS is Connected
+console.log("👍 JS Connected");
+
+// get all the card
+document.querySelectorAll(".card").forEach((item) => {
+  // for each item i.e. card, do this
+  item.addEventListener("click", (event) => {
+    // toggle class on card
+    item.classList.toggle("flipped");
+  });
+});
